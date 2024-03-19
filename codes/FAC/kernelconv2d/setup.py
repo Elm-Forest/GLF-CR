4362ea1576a20796fa44d87e8297ea80de473054
Elm Forest <62285254+Elm-Forest@users.noreply.github.com>
2024-03-20 02:43:02 +0800

    Update setup.py

--- codes/FAC/kernelconv2d/setup.py
+++ codes/FAC/kernelconv2d/setup.py
@@ -4,7 +4,7 @@ import torch
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-cxx_args = ['-std=c++11']
+cxx_args = ['-std=c++17']
 
 nvcc_args = [
     '-gencode', 'arch=compute_50,code=sm_50',
@@ -12,7 +12,10 @@ nvcc_args = [
     '-gencode', 'arch=compute_60,code=sm_60',
     '-gencode', 'arch=compute_61,code=sm_61',
     '-gencode', 'arch=compute_70,code=sm_70',
-    '-gencode', 'arch=compute_75,code=sm_75'
+    '-gencode', 'arch=compute_75,code=sm_75',
+    '-gencode', 'arch=compute_80,code=sm_80',
+    '-gencode', 'arch=compute_86,code=sm_86',
+    '-gencode', 'arch=compute_86,code=compute_86'
 ]
 
 setup(
