8d4c3dc1dec731ff6f337f1f8ce99d6f59e05ba5
Elm Forest <timbrightland@gmail.com>
2024-03-20 04:17:03 +0800

    update training method

--- codes/generic_train_test.py
+++ codes/generic_train_test.py
@@ -1,41 +1,90 @@
-import os
-import time
-from metrics import *
+import torch
+from torch.cuda.amp import GradScaler, autocast
+
+from codes.metrics import PSNR, SSIM
+
 
 class Generic_train_test():
-	def __init__(self, model, opts, dataloader):
-		self.model=model
-		self.opts=opts
-		self.dataloader=dataloader
+    def __init__(self, model, opts, dataloader, val_dataloader):
+        self.model = model
+        self.opts = opts
+        self.dataloader = dataloader
+        self.val_dataloader = val_dataloader
+        self.scaler = GradScaler()  # 初始化GradScaler
+        self.fake_batchsize = opts.fake_batchsize  # 假设opts中有一个fake_batchsize属性
 
-	def decode_input(self, data):
-		raise NotImplementedError()
+    def decode_input(self, data):
+        raise NotImplementedError()
 
-	def train(self):
-		total_steps = 0
-		print('#training images ', len(self.dataloader)*self.opts.batch_sz)
+    def validate(self):
+        """在验证集上评估模型性能。"""
+        self.model.net_G.eval()  # 设置为评估模式
+        val_loss = 0.0
+        val_ssim = 0.0
+        val_psnr = 0.0
+        with torch.no_grad():  # 关闭梯度计算
+            for _, data in enumerate(self.val_dataloader):
+                _input = self.decode_input(data)
+                with autocast():
+                    pred_Cloudfree_data = self.model.forward()
+                    loss_G = self.model.loss_fn(pred_Cloudfree_data, self.model.cloudfree_data)
+                    psnr = PSNR(pred_Cloudfree_data, self.model.cloudfree_data)
 
-		log_loss = 0
+                    ssim = SSIM(pred_Cloudfree_data, self.model.cloudfree_data)
+                val_loss += loss_G.item()
+                val_ssim += ssim.item()
+                val_psnr += psnr
+        val_loss /= len(self.val_dataloader)
+        self.model.net_G.train()  # 将模型设置回训练模式
+        return val_loss, val_psnr, val_ssim
 
-		for epoch in range(self.opts.max_epochs):
-			print(f'epoch: {epoch}')
+    def train(self):
+        total_steps = 0
+        print('#training images ', len(self.dataloader) * self.opts.batch_sz)
 
-			for _, data in enumerate(self.dataloader):
-				total_steps+=1
-				_input=self.decode_input(data)
+        log_loss = 0
+        steps_per_update = max(self.fake_batchsize // self.opts.batch_sz, 1)  # 计算梯度累积步数
+        accumulation_steps = 0  # 初始化累积步数
+        for epoch in range(self.opts.max_epochs):
+            print(f'epoch: {epoch}')
+            self.model.optimizer_G.zero_grad()  # 确保在epoch开始时梯度被清零
 
-				self.model.set_input(_input)
-				batch_loss = self.model.optimize_parameters()
-				log_loss = log_loss + batch_loss
+            for _, data in enumerate(self.dataloader):
+                total_steps += 1
+                _input = self.decode_input(data)
 
-				#=========== visualize results ============#
-				if total_steps % self.opts.log_freq==0:
-					info = self.model.get_current_scalars()
-					print('epoch', epoch, 'steps', total_steps, 'loss', log_loss/self.opts.log_freq, info)
-					log_loss = 0
+                with autocast():
+                    # 替换原有的optimize_parameters调用
+                    self.model.set_input(_input)  # 设置输入
+                    pred_Cloudfree_data = self.model.forward()  # 前向传播
+                    self.model.update_info(pred_Cloudfree_data)
+                    loss_G = self.model.loss_fn(pred_Cloudfree_data, self.model.cloudfree_data)  # 计算损失
 
-			if epoch % self.opts.save_freq==0:
-				self.model.save_checkpoint(epoch)
+                # 缩放损失并执行反向传播，但在累积足够梯度前不进行参数更新
+                self.scaler.scale(loss_G).backward()
 
-			if epoch > self.opts.lr_start_epoch_decay - self.opts.lr_step:
-				self.model.update_lr()
+                if (total_steps % steps_per_update == 0) or (_ == len(self.dataloader) - 1):
+                    # 更新模型参数
+                    self.scaler.step(self.model.optimizer_G)
+                    self.scaler.update()
+                    self.model.optimizer_G.zero_grad()  # 清空梯度
+                    accumulation_steps = 0  # 重置累积步数
+                else:
+                    accumulation_steps += 1
+
+                log_loss += loss_G.item()
+
+                # =========== visualize results ============#
+                if total_steps % self.opts.log_freq == 0:
+                    info = self.model.get_current_scalars()
+                    print('epoch', epoch, 'steps', total_steps, 'loss', log_loss / self.opts.log_freq, info)
+                    log_loss = 0
+
+            if epoch % self.opts.save_freq == 0:
+                val_loss, val_psnr, val_ssim = self.validate()  # 进行验证
+                print(f'Validation Loss after epoch {epoch}: {val_loss},  PSNR:{val_psnr},SSSIM:{val_ssim}')
+
+                self.model.save_checkpoint(epoch)
+
+            if epoch > self.opts.lr_start_epoch_decay - self.opts.lr_step:
+                self.model.update_lr()
--- codes/train_CR_net.py
+++ codes/train_CR_net.py
@@ -1,3 +1,5 @@
+import torch.utils.data
+
 from dataloader import *
 from generic_train_test import *
 from model_CR_net import *
@@ -13,7 +15,8 @@ parser.add_argument('--crop_size', type=int, default=256)
 parser.add_argument('--input_data_folder', type=str, default='K:\dataset\selected_data_folder')
 parser.add_argument('--is_use_cloudmask', type=bool, default=False)
 parser.add_argument('--cloud_threshold', type=float, default=0.2)  # only useful when is_use_cloudmask=True
-parser.add_argument('--data_list_filepath', type=str, default='E:\Development Program\Pycharm Program\dsen2-cr\csv\datasetfilelist.csv')
+parser.add_argument('--data_list_filepath', type=str,
+                    default='E:\Development Program\Pycharm Program\dsen2-cr\csv\datasetfilelist.csv')
 
 parser.add_argument('--optimizer', type=str, default='Adam', help='Adam')
 parser.add_argument('--lr', type=float, default=1e-4, help='learning rate of optimizer')
@@ -28,7 +31,9 @@ parser.add_argument('--save_model_dir', type=str, default='./checkpoints',
 parser.add_argument('--is_test', type=bool, default=False)
 
 parser.add_argument('--gpu_ids', type=str, default='0')
-
+parser.add_argument('--fake_batchsize', type=int, default=4)
+parser.add_argument('--val_bs', type=int, default=1)
+parser.add_argument('--checkpoint', type=str, default=None)
 opts = parser.parse_args()
 print_options(opts)
 
@@ -42,16 +47,19 @@ os.environ["CUDA_VISIBLE_DEVICES"] = opts.gpu_ids
 ##===================================================##
 seed_torch()
 
-train_filelist, _, _ = get_train_val_test_filelists(opts.data_list_filepath)
+train_filelist, val_filelist, _ = get_train_val_test_filelists(opts.data_list_filepath)
 
 train_data = AlignedDataset(opts, train_filelist)
-
+val_data = AlignedDataset(opts, val_filelist)
 train_dataloader = torch.utils.data.DataLoader(dataset=train_data, batch_size=opts.batch_sz, shuffle=True)
-
+val_dataloader = torch.utils.data.DataLoader(dataset=train_data, batch_size=opts.val_bs, shuffle=True)
 ##===================================================##
 ##****************** Create model *******************##
 ##===================================================##
 model = ModelCRNet(opts)
+if opts.checkpoint is not None:
+    checkpoint = torch.load(opts.checkpoint)
+    model.net_G.load_state_dict(checkpoint['network'], strict=False)
 
 
 ##===================================================##
@@ -62,4 +70,4 @@ class Train(Generic_train_test):
         return data
 
 
-Train(model, opts, train_dataloader).train()
+Train(model, opts, train_dataloader, val_dataloader).train()
